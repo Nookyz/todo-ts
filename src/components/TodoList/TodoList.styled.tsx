afe0072f9419fa6ff97a2d6bbf23d3836e983a47
Nookyz <djlive@ukr.net>
2020-06-14 17:01:39 +0300

    add style to scrollbar && fix bug with scrollbar

--- src/components/TodoList/TodoList.styled.tsx
+++ src/components/TodoList/TodoList.styled.tsx
@@ -1,8 +1,33 @@
 import styled from 'styled-components'
 
 export const MyTodoList = styled.div`
+  max-height: 800px;
   overflow-y: auto;
   overflow-x: hidden;
+  ::-webkit-scrollbar { 
+    width: 5px; 
+    height: 5px;
+  }
+  ::-webkit-scrollbar-button {
+    background-color: #666;
+  }
+  ::-webkit-scrollbar-track {  
+    background-color: #999;
+  }
+  ::-webkit-scrollbar-track-piece { 
+    background-color: #ffffff;
+  }
+  ::-webkit-scrollbar-thumb { 
+    height: 50px; 
+    background-color: #ada7c9; 
+    border-radius: 3px;
+  }
+  ::-webkit-scrollbar-corner { 
+    background-color: #999;
+  }
+  ::-webkit-resizer { 
+    background-color: #666;
+  }
 `
 export const MyTodo = styled.div`
   margin: 0 20px;
