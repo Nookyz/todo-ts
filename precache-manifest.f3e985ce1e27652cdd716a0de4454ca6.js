6cec5f613496d1af1da1741d39c247e4f4d32d48
Nookyz <djlive@ukr.net>
2020-06-16 13:05:51 +0300

    Updates

---
+++ precache-manifest.f3e985ce1e27652cdd716a0de4454ca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54311b3fccde159357ca6660fe1a0bd6",
+    "url": "/todo-ts/index.html"
+  },
+  {
+    "revision": "801f44893f5252dc8378",
+    "url": "/todo-ts/static/js/2.03209992.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/todo-ts/static/js/2.03209992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d6924e7060460757ee",
+    "url": "/todo-ts/static/js/main.4ca6a721.chunk.js"
+  },
+  {
+    "revision": "5ee5631b572ab3095480",
+    "url": "/todo-ts/static/js/runtime-main.f4e966db.js"
+  }
+]);
